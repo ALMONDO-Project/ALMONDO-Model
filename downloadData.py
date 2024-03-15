077b431eb32a08ac39205f443274028a16ad3d38
valen <valentina.pansanella@gmail.com>
2024-03-15 15:24:45 +0100

    modifiche

--- downloadData.py
+++ downloadData.py
@@ -1,111 +1,162 @@
+#import statements
 import os
-from searchtweets import collect_results, load_credentials, gen_request_parameters
-from utils import compute_max_tweets, get_oldest_tweet_id
+import json
+import tweepy
+from utils import *
 
-class UserDataDownloader:
+#class definition
+
+class UserDataDownload():
     def __init__(self,
-                 username,
-                 start_time="2023-01-01",
-                 end_time="2023-12-31",
-                 tweet_fields='text,id,attachments,created_at,lang,author_id,entities,geo',
+                 username='',
+                 tweet_fields='text,id,attachments,created_at,lang,author_id,entities,geo,public_metrics',
                  expansions='attachments.media_keys,geo.place_id',
                  media_fields='media_key,type,url,variants,preview_image_url',
-                 results_per_call=100):
-
-        self.username = username
-        self.start_time = start_time
-        self.end_time = end_time
-        self.tweet_fields = tweet_fields
-        self.media_fields = media_fields
-        self.expansions = expansions
-        self.results_per_call = results_per_call
+                 user_fields='id,username,description,public_metrics,verified',
+                 until_id = None,
+                 max_tweets = 5,
+                 limit=100):
+        # Initialize UserDataDownload object with required parameters and default values
+        self.username = username.replace('@', '')  # Remove '@' from username if present
+        self.tweet_fields = tweet_fields.split(',')  # Split tweet fields into list
+        self.media_fields = media_fields.split(',')  # Split media fields into list
+        self.expansions = expansions.split(',')  # Split expansions into list
+        self.user_fields = user_fields.split(',')  # Split user fields into list
+        self.until_id = until_id  # Set until_id for pagination
+        self.max_tweets = max_tweets  # Maximum number of tweets to retrieve
+        self.limit = limit  # API request limit
+        self.max_id = None  # Initialize max_id for pagination
+        self.tweets = {}  # Dictionary to store tweets
         
-        self.max_id = None
-        self.tweets = {}
-        self.home = os.getcwd()
-        self.tweets_path = f"{self.home}/data/out/{username}"
-        self.filename = f"{self.tweets_path}/{username}_tweets.json"
-        self.search_credentials = load_credentials(filename=f"{self.home}/cred.yaml", yaml_key="search_tweets_cred")
-        self.query = f"from:{username} -is:retweet"
-
-    def _handle_error(self, message):
-        print(f"Error: {message}")
-
+    def set_client(self, bearer_token, wait_on_rate_limit=True):
+        # Set Twitter API client with specified parameters
+        self.client = tweepy.Client(bearer_token, 
+                                    expansions = self.expansions,
+                                    media_fields = self.media_fields,
+                                    tweet_fields = self.tweet_fields,
+                                    user_fields = self.user_fields,
+                                    wait_on_rate_limit=wait_on_rate_limit)
+        
+    def set_user_data(self):
+        # Get user data using Twitter API client
+        self.user = self.client.get_user(username=self.username)
+        self.user_id = self.user.data.id
+        
+    def set_until_id(self):
+        # Set until_id for pagination based on existing data if available
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                d = json.load(file)
+            until_id = d[max(int(k) for k in d.keys())]['id'] if d else None
+            self.until_id = until_id
+        else:
+            self.until_id = None
+        
+    def set_paginator(self):
+        # Initialize paginator for fetching user tweets
+        self.paginator = tweepy.Paginator(self.client.get_users_tweets,
+                                           self.user_id,
+                                           until_id = self.until_id,
+                                           max_results=self.max_tweets)
+    
     def make_dirs(self):
         try:
-            if not os.path.exists(self.tweets_path):
-                os.makedirs(self.tweets_path)
-                print(f'Created folder: {self.tweets_path}')
-            else:
-                print(f'{self.tweets_path} already exists')
+            # Create necessary directories for storing data and logs
+            self.datapath = 'data/'
+            if not os.path.exists(self.datapath):
+                os.makedirs(self.datapath)
+            self.outdatapath = 'data/out'
+            if not os.path.exists(self.outdatapath):
+                os.makedirs(self.outdatapath)
+            self.useroutdatapath = f'data/out/{self.username}'
+            if not os.path.exists(self.useroutdatapath):
+                os.makedirs(self.useroutdatapath)
+            self.logpath = 'data/log'
+            if not os.path.exists(self.logpath):
+                os.makedirs(self.logpath)
+            self.userlogpath = f'data/log/{self.username}'
+            if not os.path.exists(self.userlogpath):
+                os.makedirs(self.userlogpath) 
         except OSError as e:
-            self._handle_error(f"Error creating directory: {self.tweets_path} - {e}")
-
-    def set_max_id(self):
-        try:
-            self.max_id = get_oldest_tweet_id(self.tweets_path)
-            if not self.max_id:
-                raise FileNotFoundError("Empty file exists")
-            print(f'Oldest tweet downloaded is {self.max_id} at {self.tweets_path}')
-        except FileNotFoundError:
-            self.max_id = None
-            print('No previous tweets downloaded, setting max_id to None')
-
-    def set_max_tweets(self, BEARER_TOKEN, n=None):
-        try:
-            left = compute_max_tweets(BEARER_TOKEN) - 1000
-            self.max_tweets = min(left, n) if n is not None else left
-            print(f'Asking to download {self.max_tweets} tweets at max')
-            if left <= 0:
-                raise NoTweetsLeftException('No tweets left to download')
-        except NoTweetsLeftException:
-            self._handle_error("No tweets left to download")
-        except Exception as e:
-            self._handle_error(f"Error setting max tweets: {e}")
+            print(e)
+            
+    def set_output_filename(self):
+        # Set output filename for storing tweets
+        self.make_dirs()
+        self.filename = os.path.join(self.useroutdatapath, f'{self.username}_tweets.json')
+            
+    def set_max_tweets(self, bearer_token, n=None):
+        # Set the maximum number of tweets to download
+        left = compute_max_tweets(bearer_token)  # Compute remaining tweets allowed
+        print(f'>>> there are {left} tweets left to download')
+        self.max_tweets = min(left, n) if n is not None else left  # Set max_tweets to minimum of remaining tweets and n
+        print(f'Asking to download {self.max_tweets} tweets at max')
+        if left <= 0:
+            print('no tweets left')
+            return None
 
     def download(self):
-        try:
-            self.search_rule = gen_request_parameters(self.query,
-                                                      results_per_call=self.results_per_call,
-                                                      tweet_fields=self.tweet_fields,
-                                                      media_fields=self.media_fields,
-                                                      expansions=self.expansions,
-                                                      max_id=self.max_id,
-                                                      start_time=self.start_time,
-                                                      end_time=self.end_time)
-
-            print(f"Collecting tweets for user {self.username}")
-
-            self.tweets = collect_results(self.search_rule, max_tweets=self.max_tweets, result_stream_args=self.search_credentials)
-
-            # Save tweets to file with timestamp
-            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            filename_with_timestamp = f"{self.home}/{self.username}_tweets_{timestamp}.json"
-            with open(filename_with_timestamp, 'w') as file:
-                json.dump(self.tweets, file)
+        self.tweets = {}  # Reset tweets dictionary
+        exc = None  # Initialize exception variable
+        try: 
+            for i, tweet in enumerate(self.paginator.flatten()):  # Iterate through paginated tweets
+                self.tweets[i] = dict(tweet)  # Store tweet data in dictionary
+                with open(f'{self.filename}', 'a+') as file:
+                    json.dump(self.tweets[i], file)  # Write tweet data to file
+            print(f'{i} tweets downloaded')
         except Exception as e:
-            self._handle_error(f"Error downloading tweets: {e}")
+            exc = str(e)  # Store the exception message
+        
+        # Update the until_id only if tweets were retrieved successfully
+        if self.tweets:
+            self.until_id = self.tweets[max(self.tweets.keys())]['id']
+        
+        # Write download log
+        d = {'user': self.username, 'user_id': self.user_id, 'last_tweet_id': self.until_id, 'exception': exc}
+        with open(f'{self.userlogpath}/downloadlog.json', 'a') as file:
+            json.dump(d, file)
+        
+        return self.tweets
+        
+    def save(self):
+        # Save tweets data to file
+        with open(f'{self.filename}', 'a') as file:
+            json.dump(self.tweets, file)
 
-    def save_tweets(self):
-        try:
-            if self.tweets:
-                try:
-                    with open(self.filename, 'r') as ifile:
-                        tweet_dict = json.load(ifile)
-                        progressive_tweet_id = max(map(int, tweet_dict.keys())) + 1
-                except (FileNotFoundError, json.JSONDecodeError):
-                    tweet_dict = {}
-                    progressive_tweet_id = 0
 
-                for tweet in self.tweets:
-                    tweet_dict[str(progressive_tweet_id)] = dict(tweet)
-                    progressive_tweet_id += 1
+''' 
+Example:
+BEARER_TOKEN = 'some string indicating the token'
+# Initialize UserDataDownload object
+user_data = UserDataDownload(auth="your_auth_token",
+                             username="desired_username")
 
-                with open(self.filename, 'w') as ofile:
-                    json.dump(tweet_dict, ofile)
-            else:
-                raise NoTweetsToSaveException('No tweets to save')
-        except NoTweetsToSaveException:
-            self._handle_error("No tweets to save")
-        except Exception as e:
-            self._handle_error(f"Error saving tweets: {e}")
+# Set up client for accessing Twitter API
+user_data.set_client(bearer_token)
+
+# Retrieve user data from Twitter
+user_data.set_user_data()
+
+# Set the ID of the oldest tweet to retrieve
+user_data.set_until_id()
+
+# Set up paginator for retrieving tweets
+user_data.set_paginator()
+
+# Create necessary directories for data storage
+user_data.set_output_filename()
+
+# Set the maximum number of tweets to retrieve based on rate limit
+user_data.set_max_tweets(bearer_token)
+
+# Download tweets and store them
+user_data.download()
+
+# Save downloaded tweets
+user_data.save()
+
+
+'''
+        
+
+        
