789b924af8c285f40866a6ee9fb2b7eec5107884
“valen” <valentina.pansanella@gmail.com>
2025-03-04 11:54:53 +0100

    added aggregate_metrics.py to create metrics files from script

---
+++ model/aggregate_metrics.py
@@ -0,0 +1,23 @@
+from classes.metrics import Metrics
+
+def main():
+    
+    """
+    Script to compute metrics for a given case study with a given number of lobbyists.
+    It creates avg_metrics.csv and a
+    
+    """      
+    
+    NLs = [0, 1, 2, 3, 4, 20] #number of lobbyists in the simulations  
+    
+    for nl in NLs:
+        basepath = f'results/balanced_budgets/{nl}_lobbyists'
+        filename = 'config.json'
+        
+        metrics = Metrics(nl=nl, basepath=basepath, filename=filename)
+        
+        metrics.compute_metrics(kind='probabilities')
+    
+                    
+if __name__ == "__main__":
+    main()
