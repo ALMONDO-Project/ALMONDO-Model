13d12bbda3fdf9b2a0176852187dade7d67f77fb
verdiana01 <verdiana.delrosso@unicam.it>
2025-07-03 14:20:01 +0200

    update __init__.py with function utils

--- model/__init__.py
+++ model/__init__.py
@@ -3,6 +3,8 @@ from .classes.simulator import ALMONDOSimulator
 from .classes.metrics import Metrics
 from .viz.opinion_distribution import OpinionDistribution
 from .viz.opinion_evolution import OpinionEvolution
+from .functions.utils import transform
+from .functions.metrics_functions import nclusters, pwdist, lobbyist_performance
 
-__all__ = ['AlmondoModel', 'ALMONDOSimulator','Metrics','OpinionDistribution','OpinionEvolution']
+__all__ = ['AlmondoModel', 'ALMONDOSimulator','Metrics','OpinionDistribution','OpinionEvolution', 'transform', 'nclusters', 'pwdist', 'lobbyist_performance']
 # __version__ = "0.1.0"
--- model/functions/__init__.py
+++ model/functions/__init__.py
@@ -1,2 +1,5 @@
 from .metrics_functions import nclusters, pwdist, lobbyist_performance
 from .utils import transform
+
+__all__ = ['transform', 'nclusters', 'pwdist', 'lobbyist_performance']
+# __version__ = "0.1.0"
